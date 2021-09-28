f1c956708d66d02977e9702d23285cc5ed532692
Gurupriyan <gurupriyan98@gmail.com>
2021-09-28 15:29:18 +0000

    Worked on the code challenge

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
 //1
 function countToFifty(){
-  
+  for (let i = 1; i <= 50; i++){
+      console.log (i);
+  }
 }
 
 //2
-function countFromOne(){
-  
+function countFromOne( num ){
+    for (let i = 1 ; i <= num; i++){
+        console.log(i);
+    }
 }
 
 //3
 function threeAndFiveInFifty(){
-  
+  for (let i = 1; i <= 50; i++){
+      if ( i % 3 === 0 || i % 5 ===0){
+          console.log(i);
+      }
+  }
 }
 
 //4
-function threeAndFive(){
-  
+function threeAndFive( num ){
+  for (let i = 1; i <= num; i++){
+      if ( i % 3 ===0 || i % 5 === 0){
+          console.log(i);
+      }
+  }
 }
 
+//5
+let newArray =[];
+function triple(array){
+  for (let i =0; i < array.length; i++){
+    newArray.push(array[i]*3);
+  }
+  return newArray;
+}
 
+console.log('Count to 50:');
+countToFifty();
+console.log('Count to the given number:');
+countFromOne(20);
+console.log('Divisible by 3 or 5 in the first 50 numbers:');
+threeAndFiveInFifty();
+console.log('Divisible by 3 or 5 from 1 to the given number:');
+threeAndFive(100);
+console.log('Tripling function:');
+console.log(triple([4, 8, 9]));
